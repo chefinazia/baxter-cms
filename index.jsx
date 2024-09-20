8da4e4634e40484b402e304265aa336e824e8186
chefinazia <rachitarora1993@gmail.com>
2024-09-20 14:31:26 +0530

    add tailwind css in code with postcss config

--- index.jsx
+++ index.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App from "./src/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const App = () => {
-   return <h3>Hello From React</h3>
-}
 
 root.render(<App />);
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const App = () => {
+    return (<div className="flex">
+       <h3 className="">Hello From React</h3>
+        <p className="text-center w-[2000px]">This is a simple React App</p>
+    </div>)
+ }
+
+export default App
