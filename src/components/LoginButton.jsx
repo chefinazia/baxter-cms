1494d326a14e881aa39833a6959911d6f4de3f74
--global <--global>
2024-09-26 11:46:37 +0530

    add common classes support in index.css

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -4,7 +4,7 @@ const LoginButton = () => {
   return (
     <>
       <button
-        className="middle rounded-[3px] duration-500 ease-in-out center bg-green-700 py-3 px-6 font-serif text-sm font-medium text-white text-ellipsis shadow-md transition-all hover:shadow-lg border border-collapse border-x-2 border-y-4 border-green-800"
+        className="btn middle rounded-[3px] duration-500 ease-in-out center font-medium text-white text-ellipsis shadow-md transition-all hover:shadow-lg border border-collapse border-x-2 border-y-4 border-green-800"
         data-ripple-light="true"
       >
         Login
