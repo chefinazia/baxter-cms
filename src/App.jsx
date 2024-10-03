83b27796b25e8b524b7b95a1e9d6a9f8c5157026
--global <--global>
2024-10-03 13:17:23 +0530

    pagination changes initail

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import LoginButton from "./components/LoginButton";
 import CalendarPatientsList from "./components/CalendarPatientsList";
 import TableClinical from "./components/TableClinical";
 import { columns, data } from "./data/clinicalData";
+import AtomPagination from "./components/AtomPagination";
+
 const App = () => {
   return (
-    <div className="divide-y-4">
+    <div className="divide-y-6">
       <div>
         <LoginButton />
       </div>
@@ -13,6 +15,12 @@ const App = () => {
         <CalendarPatientsList />
       </div>
       <TableClinical columns={columns} data={data} />
+      <AtomPagination
+        totalCount={100}
+        pageSize={5}
+        itemPerPage={2}
+        onPageChange={(e) => console.log(e)}
+      />
     </div>
   );
 };
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-const LoginButton = () => {
+const LoginButton = (className) => {
   return (
     <>
-      <button
-        className="btn middle rounded-[3px] duration-500 ease-in-out center font-medium text-white text-ellipsis shadow-md transition-all hover:shadow-lg border border-collapse border-x-2 border-y-4 border-green-800"
-        data-ripple-light="true"
-      >
-        Login
-      </button>
+      <div className={className}></div>
     </>
   );
 };
