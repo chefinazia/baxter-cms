1058773032905b139c0eb3d100595643c6e04ed6
chefinazia <rachitarora1993@gmail.com>
2024-10-03 13:09:29 +0530

    configure eslint and prettier (#6)
    * added colors in taiwind config and made changes required
    
    * changes addressed
    
    * add react-router-dom v6
    
    * remove link tag
    
    * add common classes support in index.css
    
    * change color
    
    * configure eslint and prettier
    
    * fix few breaking changes
    
    * add plus icon
    
    ---------
    
    Co-authored-by: --global <--global>

--- index.jsx
+++ index.jsx
@@ -2,22 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/App";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <App/>
-    ),
-  }
+    element: <App />,
+  },
 ]);
-
-
 
 root.render(<RouterProvider router={router} />);
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LoginButton from "./components/LoginButton";
 import CalendarPatientsList from "./components/CalendarPatientsList";
+import TableClinical from "./components/TableClinical";
+import { columns, data } from "./data/clinicalData";
 const App = () => {
   return (
     <div className="divide-y-4">
@@ -10,6 +12,7 @@ const App = () => {
       <div>
         <CalendarPatientsList />
       </div>
+      <TableClinical columns={columns} data={data} />
     </div>
   );
 };
