ac3681350abdf1c84d8ae94ed488862921f24944
Singh <suraj.b.singh@capgemini.com>
2024-10-14 16:02:04 +0530

    Navbar WIP-1

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,53 +1,73 @@
 import React from "react";
-
-const linkClasses =
-  "flex justify-center font-medium hover:bg-white hover:text-black gap-2 bg-primary-navBarItemBgColor px-4 py-2 rounded-md";
-
-const navbarItem = [
-  {
-    text: "Clinical",
-    link: "#",
-    linkDesign: linkClasses,
-  },
-  {
-    text: "Customer Service",
-    link: "#",
-    linkDesign: linkClasses,
-  },
-  {
-    text: "Patient Administration",
-    link: "#",
-    linkDesign: linkClasses,
-  },
-  {
-    text: "Adequest",
-    link: "#",
-    linkDesign: linkClasses,
-  },
-  {
-    text: "User Details",
-    link: "#",
-    linkDesign: linkClasses,
-  },
-];
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const linkClasses =
+    "flex justify-center font-medium hover:bg-white hover:text-black gap-2 bg-primary-navBarItemBgColor px-4 py-2 rounded-md";
+
+  const navbarItem = [
+    {
+      text: "Clinical",
+      link: "#",
+      subMenuFlag: false,
+      subMenuItem: [],
+    },
+    {
+      text: "Customer Service",
+      link: "#",
+      subMenuFlag: false,
+      subMenuItem: [],
+    },
+    {
+      text: "Patient Administration",
+      link: "#",
+      subMenuFlag: false,
+      subMenuItem: [],
+    },
+    {
+      text: "Adequest",
+      link: "#",
+      subMenuFlag: false,
+      subMenuItem: [],
+    },
+    {
+      text: "User Details",
+      link: "#",
+      subMenuFlag: false,
+      subMenuItem: [],
+    },
+    {
+      text: "More",
+      link: "#",
+      subMenuFlag: true,
+      subMenuItem: [],
+    },
+  ];
   return (
     <div className="flex justify-center gap-4 text-white text-sm bg-primary-navBarBgColor p-4">
-      {navbarItem.map((item) => {
+      {navbarItem.map((item, index) => {
         return (
-          <a href={item.link} className={item.linkDesign}>
-            {item.text}
-          </a>
+          <div>
+            {item.subMenuFlag ? (
+              <div className={linkClasses}>
+                <Link className={item.linkDesign} key={index} href="styles.css">
+                  {item.text}
+                </Link>
+                <img
+                  src="/src/svg/drop-down.png"
+                  className="h-4 w-4 mt-1"
+                ></img>
+              </div>
+            ) : (
+              <div className={linkClasses}>
+                <Link className={item.linkDesign} key={index} href="styles.css">
+                  {item.text}
+                </Link>
+              </div>
+            )}
+          </div>
         );
       })}
-
-      <a href="#" className={linkClasses}>
-        More
-        <span>
-          <img src="/src/svg/drop-down.png" className="h-4 w-4 mt-1"></img>
-        </span>
-      </a>
     </div>
   );
 };
